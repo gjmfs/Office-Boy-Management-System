504370f8e19961db62eb473dff96fd022b212669
gjmfs <mufeesmc28@gmail.com>
2024-12-24 17:59:13 +0530

    index page creaded for navigate between user and admin

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Order Management System</title>
+    <link rel="stylesheet" href="./Bootstrap/dist/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row row-cols-2">
+            <div class="col">
+                <a href="./admin/login.php">Admin</a>
+            </div>
+            <div class="col">
+                <a href="./user/login.php">User</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
